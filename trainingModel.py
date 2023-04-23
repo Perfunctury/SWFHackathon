00a4ea6b61462dc79eaa11f97a5d8bb0bb372c6b
Perfunctury <mcperfeunctury@gmail.com>
2023-04-23 13:05:37 -0400

    Updates for standar dev

--- trainingModel.py
+++ trainingModel.py
@@ -1,44 +1,82 @@
 import pandas
+import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 from datetime import datetime
 
-df = pandas.read_csv('SWE_Training.csv')
+#Load the training data
+unfiltered_data = pandas.read_csv('SWE_Training.csv')
 
-df = df.drop(df[df==-9999].dropna(how='all').index)
+#Filter out any rows with -9999
+unfiltered_data = unfiltered_data.drop(unfiltered_data[unfiltered_data==-9999].dropna(how='all').index)
 
 # Convert the date column to datetime format
-df['date'] = pandas.to_datetime(df['date'], format='%d-%b-%y')
+unfiltered_data['date'] = pandas.to_datetime(unfiltered_data['date'], format='%d-%b-%y')
 
 # Extract year and cumulative day of the year as new columns
-df['year'] = df['date'].dt.year
-df['day_of_year'] = df['date'].dt.dayofyear
+unfiltered_data['year'] = unfiltered_data['date'].dt.year
+unfiltered_data['day_of_year'] = unfiltered_data['date'].dt.dayofyear
 
-# Save as new data to be trained with
-df.to_csv('Modified_SWE_Training.csv', index=False)
+# Save as data to be trained with
+unfiltered_data.to_csv('Modified_SWE_Training.csv', index=False)
 
-# Step 1: Load data
-data = pandas.read_csv('Modified_SWE_Training.csv')
+# Load training data
+training_data = pandas.read_csv('Modified_SWE_Training.csv')
 
 # Separate independent variables and dependent variable
-X = df[['year', 'day_of_year', 'precip_cumulative', 'tmean']].values # Features
-y = df['swe'].values # Target
+X = training_data[['year', 'day_of_year', 'precip_cumulative', 'tmean']].values # Features
+y = training_data['swe'].values # Target
 
-# Step 3: Split data into training and testing sets
+# Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Step 4: Create a Random Forest Regressor model
+# Create a Random Forest Regressor model
 rf = RandomForestRegressor(n_estimators=100, random_state=42)
 
-# Step 5: Fit the model on the training data
+# Fit the model on the training data
 rf.fit(X_train, y_train)
 
-# Step 6: Make predictions on the testing data
+# Make predictions on the testing data
 y_pred = rf.predict(X_test)
 
-# Step 7: Evaluate the model's performance
+# Evaluate the model's performance
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 print('Mean Squared Error:', mse)
 print('R-squared:', r2)
+
+# Calculate Residuals
+residuals = y_test - y_pred
+
+# Calculate the standard deviation of the residuals
+std_dev = np.std(residuals)
+
+# Load the suspect data
+suspect_data = pandas.read_csv('Suspect_SWE_Data.csv')
+
+# Process the new data
+# Convert the date column to datetime format
+suspect_data['date'] = pandas.to_datetime(suspect_data['date'], format='%d-%b-%y')
+
+# Extract year and cumulative day of the year as new columns
+suspect_data['year'] = suspect_data['date'].dt.year
+suspect_data['day_of_year'] = suspect_data['date'].dt.dayofyear
+
+# Drop any rows with -9999
+suspect_data = suspect_data.drop(suspect_data[suspect_data==-9999].dropna(how='all').index)
+
+# Make predictions on the new data
+X_Suspect = suspect_data[['year', 'day_of_year', 'precip_cumulative', 'tmean']].values # Features
+y_Suspect = suspect_data['swe'].values # Target
+y_suspect_pred = rf.predict(X_Suspect)
+
+# Filter out entries with residuals greater than 2 standard deviations
+suspect_measurements = suspect_data[np.abs(y_suspect_pred - y_Suspect) >= 2 * std_dev]
+
+# Save the filtered data to a new file
+suspect_measurements.to_csv('suspect_measurements.csv', index=False)
+
+# Save the original data, except for the filtered entries, to a new file
+filtered_data = suspect_data[np.abs(y_suspect_pred - y_Suspect) < 2 * std_dev]
+filtered_data.to_csv('filtered_data.csv', index=False)
